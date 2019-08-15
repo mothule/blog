9a3c68989f831de4bdbdb8b1d671e317219e97e6
Motoki Kawakami <mothule.dev+github@gmail.com>
2019-08-15 22:53:18 +0900

    Deploy:

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-echo "Start deploy."
+COLOR_ESC="\e["
+COLOR_ESC_END="m"
+COLOR_YELLOW=${COLOR_ESC}33${COLOR_ESC_END}
+COLOR_PURPLE=${COLOR_ESC}35${COLOR_ESC_END}
+COLOR_CYAN=${COLOR_ESC}36${COLOR_ESC_END}
+COLOR_OFF=${COLOR_ESC}${COLOR_ESC_END}
+
+function info() {
+  printf "${COLOR_YELLOW}$1${COLOR_OFF}"
+}
+
+
+info "Start deploy."
+printf "${COLOR_YELLOW}Start deploy.${COLOR_OFF}"
 
 cd ws
 JEKYLL_ENV=production bundle exec jekyll build
