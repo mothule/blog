12185e7ae540a17c7516e8ea1d4d835c029434e6
Motoki Kawakami <mothule.dev+github@gmail.com>
2019-09-22 01:02:09 +0900

    Add compress process after download
    - Downloaded file put on /tmp directory.
    - Compressed file put on ws/assets/images

--- bin/getimage.sh
+++ bin/getimage.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-if [ $# -ne 1 ] ; then
+if [[ $# -ne 1 ]] ; then
   printf "\e[31mNeed arguments 1.\e[m\n"
   exit 1
 fi
 
+echo "Download from $1"
 outDir="./ws/assets/images"
 ext=`basename $1 | sed -r 's/^.*\.(.*)$/\1/'`
-echo "image file extension is ${ext}."
 
-if [[ -d ./ws/assets/images ]] ; then
-  http get $1 > "${outDir}/image.${ext}"
-else
+if [[ ! -d ./ws/assets/images ]] ; then
   printf "\e[31mNot found ${outDir}. recall on blog directory.\e[m\n"
+  exit 1
 fi
+
+  temp_file="/tmp/image.${ext}"
+  if [[ -e ${temp_file} ]] ; then
+    rm ${temp_file}
+  fi
+  http get $1 > ${temp_file}
+
+echo "Compress via tinypng"
+ruby bin/compress_image "${temp_file}" "${outDir}/image.${ext}"
+if [[ ! -e ${outDir}/image.${ext} ]] ; then
+  printf "\e[31mFailed compressing image.\e[m\n"
+  exit 1
+fi
+
+echo 'Successful'
+
