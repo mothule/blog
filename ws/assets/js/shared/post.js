53ea188896afc51f6b89cc7187a94de79625bd1c
Motoki Kawakami <mothule.dev+github@gmail.com>
2020-05-05 23:39:18 +0900

    Fix bug where display index when not in post page

--- ws/assets/js/shared/post.js
+++ ws/assets/js/shared/post.js
@@ -3,6 +3,6 @@
     var sidebar = $('.sidebar');
     var article = $('article');
     sidebar.height(article.height());
+    $('#side-toc-id').append('<p>目次</p>')
     toc.clone(false).appendTo($('#side-toc-id'));
 });
-
