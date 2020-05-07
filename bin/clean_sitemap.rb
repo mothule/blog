7799ccf121f6b86c5387166c5b29d3c61a9d4e3f
Motoki Kawakami <mothule.dev+github@gmail.com>
2020-05-07 22:19:44 +0900

    Add clean_sitemap script

---
+++ bin/clean_sitemap.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'active_support/core_ext/hash'
+require 'erb'
+
+sitemap_path = './ws/_site/sitemap.xml'
+
+raise 'Not found sitemap.xml' unless File.exist?(sitemap_path)
+
+xml = Hash.from_xml(File.open(sitemap_path))
+xml['urlset']['url'].reject! do |url|
+  url['loc'].match?(/page\d/)
+end
+
+url_info_list = xml['urlset']['url'].map do |url|
+  { url: url['loc'], lastmod: url['lastmod'] }
+end
+template = File.read('./bin/src/sitemap.xml.erb')
+erb = ERB.new(template, nil, '-')
+string = erb.result(binding)
+File.write(sitemap_path, string)
