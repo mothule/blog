c76820907b69522f2e0d0e878ecfb516fa6057b1
Motoki Kawakami <mothule.dev+github@gmail.com>
2020-05-10 17:11:28 +0900

    Improve post layout
    - 日付場所の右寄せ
    - Pocketボタンの追加
    - シェアボタン群をサイドのTOC下に追加

--- ws/assets/js/shared/post.js
+++ ws/assets/js/shared/post.js
@@ -1,11 +1,28 @@
+---
+---
 ;$(function(){
     var windowWidth = $(window).width();
     if(windowWidth > 991) {
-        var toc = $('.post-content-toc');
-        var sidebar = $('.sidebar');
-        var article = $('article');
-        sidebar.height(article.height());
-        $('#side-toc-id').append('<p>目次</p>')
-        toc.clone(false).appendTo($('#side-toc-id'));
+        buildTableOfContentsOnSide()
+        buildShareButtonsOnSide()
+    }
+
+    // Table of Contents作成
+    function buildTableOfContentsOnSide() {
+      var toc = $('.post-content-toc');
+      var sidebar = $('.sidebar');
+      var article = $('article');
+      sidebar.height(article.height());
+      $('#side-toc-id').append('<p>目次</p>')
+      toc.clone(false).appendTo($('#side-toc-id'));
+    }
+
+    // シェアボタン群作成
+    function buildShareButtonsOnSide() {
+      var hatena_bookmark_button = `
+      <li>{% include hatena_bookmark_button.html %}</li>
+      <li>{% include pocket_button.html %}</li>
+      `;
+      $('#side-share-id>ul').append(hatena_bookmark_button);
     }
 });
