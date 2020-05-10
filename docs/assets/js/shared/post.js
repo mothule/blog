ac255ca7a2910e439ff74c8eea8dd47e3fbf95e9
Motoki Kawakami <mothule.dev+github@gmail.com>
2020-05-10 21:01:20 +0900

    Deploy:

--- docs/assets/js/shared/post.js
+++ docs/assets/js/shared/post.js
@@ -2,7 +2,7 @@
     var windowWidth = $(window).width();
     if(windowWidth > 991) {
         buildTableOfContentsOnSide()
-        buildShareButtonsOnSide()
+        showShareButtonsOnSide()
     }
 
     // Table of Contents作成
@@ -16,17 +16,7 @@
     }
 
     // シェアボタン群作成
-    function buildShareButtonsOnSide() {
-      var hatena_bookmark_button = `
-      <li><a href="https://b.hatena.ne.jp/entry/" class="hatena-bookmark-button" data-hatena-bookmark-layout="basic-label-counter" data-hatena-bookmark-lang="ja" title="このエントリーをはてなブックマークに追加"><img src="https://b.st-hatena.com/images/v4/public/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a>
-</li>
-      `;
-
-      var pocket_button = `
-        <li><span style="display: inline-block;"><a data-pocket-label="pocket" data-pocket-count="horizontal" class="pocket-btn" data-lang="en"></a></span>
-</li>
-      `;
-      $('#side-share-id>ul').append(pocket_button);
-      $('#side-share-id>ul').append(hatena_bookmark_button);
-  }
+    function showShareButtonsOnSide() {
+      $('#side-share-id').css('display', 'block');
+    }
 });
