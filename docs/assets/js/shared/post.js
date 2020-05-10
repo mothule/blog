fb440e4afd72e0e1a25d9e46177855502db4d6eb
Motoki Kawakami <mothule.dev+github@gmail.com>
2020-05-10 17:13:01 +0900

    Deploy:

--- docs/assets/js/shared/post.js
+++ docs/assets/js/shared/post.js
@@ -1,11 +1,28 @@
 ;$(function(){
     var windowWidth = $(window).width();
     if(windowWidth > 991) {
-        var toc = $('.post-content-toc');
-        var sidebar = $('.sidebar');
-        var article = $('article');
-        sidebar.height(article.height());
-        $('#side-toc-id').append('<p>目次</p>')
-        toc.clone(false).appendTo($('#side-toc-id'));
+        buildTableOfContentsOnSide()
+        buildShareButtonsOnSide()
+    }
+
+    // Table of Contents作成
+    function buildTableOfContentsOnSide() {
+      var toc = $('.post-content-toc');
+      var sidebar = $('.sidebar');
+      var article = $('article');
+      sidebar.height(article.height());
+      $('#side-toc-id').append('<p>目次</p>')
+      toc.clone(false).appendTo($('#side-toc-id'));
+    }
+
+    // シェアボタン群作成
+    function buildShareButtonsOnSide() {
+      var hatena_bookmark_button = `
+      <li><a href="https://b.hatena.ne.jp/entry/" class="hatena-bookmark-button" data-hatena-bookmark-layout="basic-label-counter" data-hatena-bookmark-lang="ja" title="このエントリーをはてなブックマークに追加"><img src="https://b.st-hatena.com/images/v4/public/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a>
+</li>
+      <li><span style="display: inline-block;"><a data-pocket-label="pocket" data-pocket-count="horizontal" class="pocket-btn" data-lang="en"></a></span>
+</li>
+      `;
+      $('#side-share-id>ul').append(hatena_bookmark_button);
     }
 });
